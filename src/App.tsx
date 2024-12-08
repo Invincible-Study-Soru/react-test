3e076829015d2bfe47867708899b5f63c3f3111d
Soru <chansolchoe9216@gmail.com>
2024-12-08 17:47:28 +0900

    [chore] Show sample chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import ReactApexChart from "react-apexcharts";
+
 function App() {
-  return <>Hello everyone!</>;
+  return (
+    <>
+      <ReactApexChart
+        type="bar"
+        width="500"
+        options={{
+          chart: {
+            id: "basic-bar",
+          },
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+          },
+        }}
+        series={[
+          {
+            name: "series-1",
+            data: [30, 40, 45, 50, 49, 60, 70, 91],
+          },
+        ]}
+      />
+    </>
+  );
 }
 
 export default App;
